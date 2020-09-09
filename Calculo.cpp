6795ad67382d5496e08c34c54e434f17251c8006
AngelC32 <70071489+AngelC32@users.noreply.github.com>
2020-09-09 10:29:01 -0500

    Update Calculo.cpp

--- Calculo.cpp
+++ Calculo.cpp
@@ -2,8 +2,7 @@
  * nomPrograma:
  * descripcion:
  *
- * *//**Todavia falta completar o modificar*
-      *Agregar Ordenar vector**//*
+ * *//**Todavia falta completar o modificar*//*
  * autor: ROMAN CESPEDES, Samuel Aaron
  *        TORRE ARTEAGA, Alexander
  *        CARMEN CRUZATTI, Angel Gerardo
@@ -19,6 +18,8 @@ using namespace std;
 
 struct CALCULO{
    int codAlu;
+   char nomAlu[40];
+   char escuela[4];
    float promFinal;
 };
 
@@ -41,30 +42,35 @@ struct EXFINAL{
 	float exfin=0;
 };
 
+void crearVector(int *dp);
 void RecuperarNotas(PRACTICA x[], EXPARCIAL y[], EXFINAL z[], int *dx, int *dy, int *dz);
-void CalculoPromedio(PRACTICA x[], EXPARCIAL y[], EXFINAL z[], int dx, int dy, int dz);
+void CalculoPromedio(PRACTICA x[], EXPARCIAL y[], EXFINAL z[],CALCULO prom[], int dx, int dy, int dz,int *dp);
+
+void encabezado();
+void raya1();
+void raya2();
 
 int main()
 {
    PRACTICA p[MAX];
    EXPARCIAL par[MAX];
    EXFINAL fin[MAX];
-   int np,npar,nfin;
+   CALCULO prom[MAX];
+   int np,npar,nfin,nprom;
    RecuperarNotas(p,par,fin,&np,&npar,&nfin);
-   CalculoPromedio(p,par,fin,np,npar,nfin);
-
+   CalculoPromedio(p,par,fin,prom,np,npar,nfin,&nprom);
    system("pause");
    return(0);
 }
-
+void crearVector(int *dp){
+   *dp=-1;
+}
 void RecuperarNotas(PRACTICA x[], EXPARCIAL y[], EXFINAL z[], int *dx, int *dy, int *dz){
    FILE *A,*B,*C,*D;
    int i;
    A= fopen("Practica.dat","r");
    B= fopen("Parcial.dat","r");
    C= fopen("Final.dat","r");
-   //D= fopen("PromedioFinal.dat","w");
-
    PRACTICA p;
    EXPARCIAL par;
    EXFINAL fin;
@@ -103,10 +109,11 @@ void RecuperarNotas(PRACTICA x[], EXPARCIAL y[], EXFINAL z[], int *dx, int *dy, 
    fclose(C);
 }
 
-void CalculoPromedio(PRACTICA x[], EXPARCIAL y[], EXFINAL z[], int dx, int dy, int dz){
-   CALCULO prom[MAX];
+void CalculoPromedio(PRACTICA x[], EXPARCIAL y[], EXFINAL z[],CALCULO prom[], int dx, int dy, int dz,int *dp){//falta modificar
    int i=0;
    float suma=0;
+   int codigo;
+
    if(dx==dy&&dx==dz){
       for(i=0;i<dx;i++){
          suma=x[i].promedio+y[i].expar + z[i].exfin;// registros previamente ordenados
@@ -115,5 +122,44 @@ void CalculoPromedio(PRACTICA x[], EXPARCIAL y[], EXFINAL z[], int dx, int dy, i
          prom[i].promFinal=suma;
          suma=0;
       }
+
    }
+}
+
+void mostrarPromedios(CALCULO prom[], int dp){
+   int i;
+   if(dp>0){
+      for(i=0;i<dp;i++){
+         encabezado();
+         printf("%3d\t%-12d%-30s%-10s%-10.1f\n",i+1,prom[i].codAlu,prom[i].nomAlu,prom[i].escuela,prom[i].promFinal);
+
+      }
+   }else{
+      printf("\nVector vacioooo...!\n");
+   }
+   system("pause");
+}
+
+void encabezado(){
+   system("cls");
+   char title[]="REGISTRO DE PROMEDIOS FINALES";
+   char num[]="NUMERO";
+   char cod[]="CODIGO";
+   char nom[]="APELLIDOS, Nombres";
+   char escuela[]="ESCUELA";
+   char nota[]="NOTA";
+   printf("\n\t\t%s\n\n",title);
+   raya1();
+   printf("%-6s\t%-12s%-30s%-14s%-5s\n",num,cod,nom,escuela,nota);
+   raya2();
+}
+
+void raya2()
+{
+   printf("-------------------------------------------------------------------------------------------------------------\n");
+}
+
+void raya1()
+{
+   printf("=============================================================================================================\n");
 }
