fa4e0fa0baf4e0a56519b26bf63ea22dff4332f1
AngelC32 <70071489+AngelC32@users.noreply.github.com>
2020-09-17 20:31:39 -0500

    Update Practica.cpp

--- Practica.cpp
+++ Practica.cpp
@@ -24,20 +24,46 @@ struct PRACTICA{
    int prac[10];
    int promedio;
 };
+//OPERACIONES BASICAS CON VECTORES
 void crearVectorPc(int *dx);
 void leerVectorPc(PRACTICA p[MAX],int *dx);
 void mostrarVectorPc(PRACTICA p[MAX],int dx);
 int buscarPcxCodAlu(PRACTICA p[MAX],int dx,int cod);
+void buscarPracticasporCodigoAlumno(PRACTICA p[MAX], int dx);
 int buscarPcxCodCurso(PRACTICA p[MAX],int dx,int cod);
+void buscarPracticasporCodigoCurso(PRACTICA p[MAX], int dx);
 void editarporCodigoPc(PRACTICA p[MAX], int dx);
 void editarPc(PRACTICA p[MAX],int dx);
-void insertarPosPc(PRACTICA p[MAX],int *dx,int pos);
-void fileSalvarPractica(FILE *F,PRACTICA p[MAX],int dx);
-void fileRecuperarPractica(FILE *F);
+void insertarPracticasporPosicion(PRACTICA p[MAX],int *dx);
+void insertarPracticasporValor(PRACTICA p[MAX],int *dx);
+void eliminarPracticasporPosicion(PRACTICA p[MAX],int *dx);
+void eliminarPracticasporValor(PRACTICA p[MAX],int *dx);
+
+//MENU
 void menu();
+void menu2(PRACTICA fin[MAX],int *dx,int mod);
 void encabezado2(char title[]);
 void raya3();
 void raya4();
+
+//ARCHIVOS
+
+//Escribir
+void fileSalvarPractica(FILE *F,PRACTICA p[MAX],int dx);
+//Leer
+void fileRecuperarPractica(FILE *F);
+//Buscar
+void fileBuscarPractica(FILE *F);
+//Filtrar
+void fileFiltrarPractica(FILE *F);
+//Eliminar
+void fileEliminarPractica(FILE *F);
+//Editar
+void fileEditarPractica(FILE *FF);
+//Copiar
+void fileCopiarPractica(FILE *F);
+//Ordenar
+void fileOrdenarPractica(FILE *F);
 
 int main (){
    menu();
@@ -59,19 +85,27 @@ void menu(){
    system("cls");
    printf(" \n\n\t\tSISTEMA DE NOTAS DE PRACTICA\n\n");
    printf("\t0. TERMINAR \n\n");
-   printf("\t1. CREAR\n");
-   printf("\t2. LEER\n");
-   printf("\t3. MOSTRAR\n");
-   printf("\t4. BUSCARxCodCur\n");
-   printf("\t5. BUSCARxCodAlu\n");
-   printf("\t6. EDITAR\n");
-   printf("\t7. INSERTAR\n");
-   printf("\t8. SALVAR\n");
-   printf("\t9. RECUPERAR\n");
+   printf("\t1. INGRESO DE DATOS\n");
+   printf("\t2. MOSTRAR\n");
+   printf("\t3. BUSCAR\n");
+   printf("\t4. EDITAR\n");
+   printf("\t5. INSERTAR\n");
+   printf("\t6. ORDENAR POR CODIGO\n");
+   printf("\t7. ORDENAR POR NOTA\n");
+   printf("\t8. ELIMINAR\n");
+   printf("\t\tOPERACIONES CON ARCHIVOS\n");
+   printf("\t9.  SALVAR\n");
+   printf("\t10. RECUPERAR\n");
+   printf("\t11. Buscar\n");
+   printf("\t12. Filtrar\n");
+   printf("\t13. Eliminar\n");
+   printf("\t14. Editar\n");
+   printf("\t15. Copiar\n");
+   printf("\t16. Ordenar\n");
    do{
       printf("\nDigite su opcion ---> ");
       scanf("%d",&opc);
-   }while(opc<0 || opc>9);
+   }while(opc<0 || opc>16);
         system("cls");
         switch(opc){
             case 0:
@@ -79,51 +113,105 @@ void menu(){
                 printf("SALIO DEL PROGRAMA\n");
                 system("pause");
                 exit(0);
-            case 1:
-                crearVectorPc(&np);
-                break;
-            case 2:
+            case 1://LEER
                 leerVectorPc(p,&np);
                 break;
-            case 3:
+            case 2://MOSTRAR
                 mostrarVectorPc(p,np);
                 break;
-            case 4:
-            {
-                int cod;
-                printf("CODIGO: ");
-                scanf("%d",&cod);
-                buscarPcxCodCurso(p,np,cod);
-            }
-            break;
-            case 5:
-            {
-                int cod;
-                printf("CODIGO: ");
-                scanf("%d",&cod);
-                buscarPcxCodAlu(p,np,cod);
-            }
-            break;
-            case 6:
-                editarPc(p,np);
+            case 3://BUSCAR
+                menu2(p,&np,opc);
                 break;
+            case 4://EDITAR
+               editarPc(p,np);
+               break;
+            case 5://INSERTAR
+               menu2(p,&np,opc);
+               break;
+            case 6:
+               break;
             case 7:
-            {
-
-               int pos=0;
-               insertarPosPc(p,&np,pos);
-            }
-            break;
+               break;
             case 8:
+               menu2(p,&np,opc);
+               break;
+            case 9:
                 fileSalvarPractica(FPC,p,np);
                 break;
-            case 9:
+            case 10:
                 fileRecuperarPractica(FPC);
                 break;
+            case 11:
+               fileBuscarPractica(FPC);
+               break;
+            case 12:
+               fileFiltrarPractica(FPC);
+               break;
+            case 13:
+               fileEliminarPractica(FPC);
+               break;
+            case 14:
+               fileEditarPractica(FPC);
+               break;
+            case 15:
+               fileCopiarPractica(FPC);
+               break;
+            case 16:
+               fileOrdenarPractica(FPC);
+               break;
             default:
                 printf("Opcion invalida....!\n");
         }
    }while(opc!=0);
+}
+
+void menu2(PRACTICA p[MAX],int *dx,int mod){
+  int opc;
+   system("cls");
+   if(mod==3){
+      printf("\tBUSCAR NOTAS DE PRACTICAS\n");
+   }else if(mod==5){
+      printf("\tINSERTAR NOTAS DE PRACTICAS\n");
+   }else{
+      printf("\tELIMINAR NOTAS DE PRACTICAS");
+   }
+
+   printf("\n0. TERMINAR\n\n");
+
+   if(mod==3){
+      printf("\t1.CODIGO ALUMNO\n");
+      printf("\t2.CODIGO CURSO\n");
+   }else{
+      printf("\t1.POSICION\n");
+      printf("\t2.VALOR\n");
+   }
+
+   do{
+      printf("\nDigite su opcion ---> ");
+      scanf("%d",&opc);
+   }while(opc<0 || opc>2);
+   switch (opc)
+   {
+   case 1:
+      if(mod==3){
+         buscarPracticasporCodigoAlumno(p,*dx);
+      }else if(mod==5){
+         insertarPracticasporPosicion(p,dx);
+      }else{
+         eliminarPracticasporPosicion(p,dx);
+      }
+      break;
+
+   case 2:
+      if(mod==3){
+         buscarPracticasporCodigoCurso(p,*dx);
+      }else if(mod==5){
+         insertarPracticasporValor(p,dx);
+      }else{
+         eliminarPracticasporValor(p,dx);
+      }
+      break;
+   }
 }
 
 void crearVectorPc(int *dx){
@@ -185,7 +273,7 @@ void encabezado2(char title[]){
    system("cls");
    char num[]="NUMERO";
    char cod[]="CODIGO";
-   char promedio[]="PROMEDIO";
+   char promedio[]="NOTA";
    printf("\t\t%s\n\n",title);
    raya3();
    printf("%-6s\t%-12s%-5s\n",num,cod,promedio);
@@ -216,27 +304,72 @@ int buscarPcxCodAlu(PRACTICA p[MAX],int dx,int cod){
          pos = i;
       }
    }
-
    return pos+1;
 }
-void insertarPosPc(PRACTICA p[MAX],int *dx,int pos){
+
+void buscarPracticasporCodigoCurso(PRACTICA p[MAX], int dx){
+   int cd,i,comp=0;//comp es un valor bandera
+   char titulo[]="\n\nREPORTE DE PROMEDIO DE PRACTICAS";
+   printf("\n\nCODIGO : ");
+   scanf("%i",&cd);
+   cout <<"\n\n";
+   for(i=0;i<dx;i++){
+      if(p[i].codCur==cd){
+         encabezado2(titulo);
+         printf("%3d\t%-12d%-10d\n",i+1,p[i].codCur,p[i].promedio);
+         comp=1;
+      }
+   }
+   cout <<"\n\n";
+   if(comp==0){
+      printf("No se encontraron cursos con el codigo ingresado %d\n",cd);
+   }
+   system("pause");
+}
+
+void buscarPracticasporCodigoAlumno(PRACTICA p[MAX], int dx){
+   int cd,i,comp=0;//comp es un valor bandera
+   char titulo[]="\n\nREPORTE DE PROMEDIO DE PRACTICAS";
+   printf("\n\nCODIGO : ");
+   scanf("%i",&cd);
+   cout <<"\n\n";
+   for(i=0;i<dx;i++){
+      if(p[i].codAlu==cd){
+         encabezado2(titulo);
+         printf("%3d\t%-12d%-10.1d\n",i+1,p[i].codAlu,p[i].promedio);
+         comp=1;
+      }
+   }
+   cout <<"\n\n";
+   if(comp==0){
+      printf("No se encontraron cursos con el codigo ingresado %d\n",cd);
+   }
+   system("pause");
+}
+void insertarPracticasporPosicion(PRACTICA p[MAX],int *dx){
    int i,j,n,suma=0;
+   int posi,cod;
    mostrarVectorPc(p,*dx);
-   printf("POSICION: ");
-   scanf("%i",&pos);
    n = *dx;
    n = n + 1;
    i = *dx;
+   printf("\n\nPOSICION: ");
+   scanf("%i",&posi);
    if(n<MAX){
-      if(pos-1>-1 && pos-1<*dx+1){
-         while(i>pos-1){
-            p[i] = p[i-1];
-            i = i-1;
-         }
-         fflush(stdin);
-         printf("\n Codigo de Alumno : ");
-         cin>>p[i].codAlu;
-         cout <<"\n Codigo de Curso ---> ";
+      if(posi-1>-1 && posi-1<*dx+1){
+         while(i>posi-1){
+               p[i] = p[i-1];
+               i--;
+            }
+         *dx=n;
+         printf("\n**Registro de PRACTICAS: \n");
+         printf("\n\tAlumno:  %d\n", posi);
+
+         cout <<" Codigo ---> ";
+         cin >>cod;
+
+         p[i].codAlu=cod;
+         cout <<" Codigo de Curso ---> ";
          cin>>p[i].codCur;
          for(j=0;j<10;j++){
             printf("\n\tPractica %d: ",j+1);
@@ -247,32 +380,41 @@ void insertarPosPc(PRACTICA p[MAX],int *dx,int pos){
          p[i].promedio=suma/10;
          suma=0;
          printf("\n\tEl promedio de practicas del alumno %d : es %d\n\n",i+1,p[i].promedio);
-         fflush(stdin);
-         *dx=n;
-
-      printf("\nDatos INSERTADOS en posicion %d \n\n ",pos);
+         printf("\nDatos INSERTADOS en posicion %d \n\n ",posi);
       }
       else{
-         printf("La posicion %d no existe en el vector...\n", pos);
+         printf("La posicion %d no existe en el vector...\n", posi);
       }
    }
-   else{
+    else{
       printf("Dimension fuera de rango ...\n");
    }
    system("pause");
 }
+void insertarPracticasporValor(PRACTICA p[MAX],int *dx){
+   int posi,i,j,n,suma;
+   int codigo;
+   n=*dx;
+   posi = 1;
+   mostrarVectorPc(p,*dx);
+   if(n<MAX){
+      printf("\n\nCODIGO DE ALUMNO: ");
+      scanf("%i",&codigo);
 
+      for(i=0;i<*dx;i++){
+         if(codigo>p[i].codAlu){
+               posi+=1;
+         }
+      }
+      printf("\n\nA continuacion ingrese los datos de las practicas...\n");
 
-void editarporCodigoPc(PRACTICA p[MAX], int dx){
-   int cod,i,j,comp=0,suma=0;//comp es un valor bandera
-   mostrarVectorPc(p,dx);
-   printf("\n\nCODIGO: ");
-   scanf("%i",&cod);
-   for(i=0;i<dx;i++){
-      if(p[i].codAlu==cod){
-         comp=1;
-         cout <<"\n\tCodigo "<<i+1<<". ";
-         for(j=0;j<10;j++){
+      printf("\n**Registro de PRACTICAS: \n");
+      printf("\n\tAlumno:  %d\n", posi);
+
+      p[posi-1].codAlu=codigo;
+      cout <<" Codigo de Curso ---> ";
+      cin>>p[i].codCur;
+      for(j=0;j<10;j++){
             printf("\n\t\tPractica %d: ",j+1);
             cin>>p[i].prac[j];
             cout<<endl;
@@ -281,12 +423,72 @@ void editarporCodigoPc(PRACTICA p[MAX], int dx){
          p[i].promedio=suma/10;
          suma=0;
          printf("\n\tEl promedio de practicas del alumno %d : es %d\n\n",i+1,p[i].promedio);
+
+      printf("\nDatos INSERTADOS en posicion %d \n\n ",posi);
+      *dx+=1;
+   }
+   else{
+      printf("Dimension fuera de rango ...\n");
+   }
+}
+
+void eliminarPracticasporPosicion(PRACTICA p[MAX],int *dx){
+   int i,n,posi;
+   mostrarVectorPc(p,*dx);
+   printf("\n\nPOSICION: ");
+   scanf("%i",&posi);
+   n=*dx;
+   n=n-1;
+   i=posi-1;
+   if(posi-1>-1 && posi-1<*dx){
+      *dx=n;
+      while(i<n){
+         p[i] = p[i+1];
+         i = i+1;
+      }
+      printf("\nDatos de las Practicas de la posicion %d ELIMINADOS\n\n ",posi);
+   }
+   else{
+      printf("La posicion %d no existe en el vector...\n", posi);
+   }
+}
+
+void eliminarPracticasporValor(PRACTICA p[MAX],int *dx){
+   int n,i,j,pos;
+   bool band;
+   int codigo;
+   n = *dx;
+   band=false;
+
+   system("cls");
+   printf("\n\nCODIGO DE ALUMNO:\n");
+   cout <<" Codigo ---> ";
+   cin>>codigo;
+   cout<<endl;
+
+   if(*dx>0){
+      for(i=0;i<n;i++){
+         if(codigo==p[i].codAlu){
+            pos = i;
+            band = true;
+         }
+      }
+      if(band){
+         for(j=pos;j<n;j++){
+            p[j]=p[j+1];
+         }
+         printf("Dato eliminado...!\n");
+         *dx=n-1;
+         //muestra resultados
+         mostrarVectorPc(p,*dx);
+      }
+      else{
+         printf("No existe ese dato...!\n");
       }
    }
-   if(comp==0){
-      printf("No se encontro alumnos con el codigo ingresado %d\n",cod);
+   else{
+      printf("Vacio...!\n");
    }
-	system("pause");
 }
 
 void editarPc(PRACTICA p[MAX],int dx){
@@ -315,10 +517,7 @@ void editarPc(PRACTICA p[MAX],int dx){
 	system("pause");
 
 }
-
-//IMPLEMENTAR
-
-/*void fileSalvarPractica(FILE *F,PRACTICA p[MAX],int dx){
+void fileSalvarPractica(FILE *F,PRACTICA p[MAX],int dx){
     F = fopen("Practica.dat","a+");
     if(F == NULL){
         printf("Ha ocurrido un error...!\n");
@@ -333,12 +532,12 @@ void editarPc(PRACTICA p[MAX],int dx){
     printf("Datos guardados...!\n");
     fclose(F);
     system("pause");
-}*/
-/*void fileRecuperarPractica(FILE *F){
+}
+void fileRecuperarPractica(FILE *F){
     int i=0;
     PRACTICA pp;
     F = fopen("Practica.dat","r+");
-    char titulo[]="\n\nREPORTE DE PRACTICAS";
+    char titulo[]="\n\nREPORTE DE Practicas";
 
     if(F==NULL){
         printf("Ha ocurrido un error...!\n");
@@ -356,8 +555,8 @@ void editarPc(PRACTICA p[MAX],int dx){
 
     fclose(F);
     system("pause");
-}*/
-/*void fileBuscarPromPractica(FILE *F){
+}
+void fileBuscarPromPractica(FILE *F){
    F = fopen("Practica.dat","a+");
    char titulo[]="\n\nREPORTE DE PROMEDIO DE PRACTICAS";
    PRACTICA k ;
@@ -367,8 +566,7 @@ void editarPc(PRACTICA p[MAX],int dx){
    scanf("%d",&Codigo);
    // aqui siempre debe empezar el ciclo de lectura
    // y fread() regresa siempre cuantas estructuras leyo
-
-   encabezado(titulo);
+   encabezado2(titulo);
    fread(&k,sizeof(k),1,F);
    while(!feof(F))
    { // desplegando Registro Buscado
@@ -379,10 +577,10 @@ void editarPc(PRACTICA p[MAX],int dx){
       fread(&k, sizeof(k), 1, F);
    }; // aqui termina while
    // no olvidar cerrar archivo y siempre fuera de while
-   raya1();
+   raya3();
    fclose(F);
-}*/
-/*void fileFiltrarPromPractica(FILE *F){
+}
+void fileFiltrarPromPractica(FILE *F){
    F = fopen("Practica.dat","a+");
    char titulo[]="\n\nREPORTE DE PROMEDIO PRACTICAS";
    PRACTICA k;
@@ -392,7 +590,7 @@ void editarPc(PRACTICA p[MAX],int dx){
     scanf("%d",&Codigo);
     // aqui siempre debe empezar el ciclo de lectura
     // y fread() regresa siempre cuantas estructuras leyo
-    encabezado(titulo);
+    encabezado2(titulo);
     fread(&k,sizeof(k),1,F);
     while(!feof(F))
     {
@@ -405,15 +603,15 @@ void editarPc(PRACTICA p[MAX],int dx){
         }
         fread(&k, sizeof(k), 1, F);
     }; // aqui termina while
-    raya1();
+    raya3();
     if(Hallado == 0)
     {
     printf("Ninguno cumple con esa Condicion\n\n");
     }
     // no olvidar cerrar archivo y siempre fuera de while
     fclose(F);
-}*/
-/*void fileEliminarPromPractica(FILE *FF){
+}
+void fileEliminarPromPractica(FILE *FF){
     FF = fopen("Practica.dat","a+");
     FILE *HH;
     PRACTICA k;
@@ -451,9 +649,9 @@ void editarPc(PRACTICA p[MAX],int dx){
     else{
         printf("Registro eliminado\n\n");
     }
-}*/
+}
 
-/*void fileEditarPractica(FILE *FF)
+void fileEditarPractica(FILE *FF)
 {
     FF = fopen("Practica.dat","a+");
     FILE *HH;
@@ -462,7 +660,6 @@ void editarPc(PRACTICA p[MAX],int dx){
     // cargando codigo a buscar
     printf("Codigo de alumno a Editar ---> ");
     scanf("%d",&Codigo);
-
    HH = fopen("Temporal.dat","a+");
     if (HH == NULL)
     {
@@ -498,7 +695,6 @@ void editarPc(PRACTICA p[MAX],int dx){
                         scanf("%d",&m.codAlu);
                         break;
                     case 2:
-
                         for(j=0;j<10;j++){
                         printf("\n\t\tPractica %d: ",j+1);
                         cin>>k[i].prac[j];
@@ -512,7 +708,6 @@ void editarPc(PRACTICA p[MAX],int dx){
                         default:
                         printf("Opcion incorrecta!\n");
                 }
-
             }
             if(desea=='S' || desea=='s'){
                 fwrite(&m,sizeof(m),1,HH);
@@ -520,7 +715,6 @@ void editarPc(PRACTICA p[MAX],int dx){
             else{
                 fwrite(&k, sizeof(k), 1, HH);
             }
-
         }
         else
         {
@@ -543,30 +737,27 @@ void editarPc(PRACTICA p[MAX],int dx){
     else{
         printf("Registro Actualizado");
     }
-}*/
-/*void fileCopiarPractica(FILE *F){
+}
+void fileCopiarPractica(FILE *F){
     F = fopen("Practica.dat","a+");
     FILE *H;
     PRACTICA k;
     H = fopen("CopiaPractica.dat","a+");
-
     fread(&k,sizeof(k),1,F);
-
     while(!feof(F)){
         fwrite(&k,sizeof(k),1,H);
         fread(&k,sizeof(k),1,F);
     }
     fclose(H);
     fclose(F);
-}*/
-/*void fileOrdenarPractica(FILE *F){
+}
+void fileOrdenarPractica(FILE *F){
     F = fopen("Final.dat","a+");
     PRACTICA k[MAX];
     PRACTICA t;
     FILE *H;
     H=fopen("Temporal.dat","a+");
     int i=0,aux=0,j=0,n;
-
     fread(&k[i],sizeof(PRACTICA),1,F);
     i++;
     while(!feof(F)){
@@ -575,7 +766,6 @@ void editarPc(PRACTICA p[MAX],int dx){
     }
     n=i-1;
     fclose(F);
-
     for(i=0;i<n-1;i++){
          aux=i;
          for(j=i+1;j<n;j++){
@@ -594,10 +784,8 @@ void editarPc(PRACTICA p[MAX],int dx){
     }
     //Cierra el archivo
     fclose(H);
-
     int rem=remove("Practica.dat");
     printf("rem=%d\n",rem);
     int ren=rename("Temporal.dat","Practica.dat");
     printf("ren=%d\n",ren);
-
-}*/
+}
